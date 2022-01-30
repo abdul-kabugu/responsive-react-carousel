f7941d2e3639aa31b513d697a45d60af6bb6cf92
ABDUL-KABUGU <kabuguabdul2@gmail.comL>
2022-01-31 01:13:08 +0300

    publishing carousel v1

--- src/components/Navbar.jsx
+++ src/components/Navbar.jsx
@@ -1,8 +1,53 @@
-import React from 'react';
+import React,{useState} from 'react';
+/*import "react-responsive-carousel/lib/styles/carousel.min.css"; // requires a loader
+import { Carousel } from 'react-responsive-carousel';*/
+import Slider from 'react-slick'
+import 'slick-carousel/slick/slick.css'
+import 'slick-carousel/slick/slick-theme.css'
+import { hotelCards } from '../HotelCards';
+
+import "./nav.css"
 
 const Navbar = () => {
-    return <div>
-        this is y navbar section  i will be creating  a beutiful navbara keep woathing
+    const [slideRef, setSlideRef] = useState(null)
+    const sliderSetting = {
+        slidesToShow: 3,
+        slidesToScroll: 1,
+        arrows: false,
+        dots: true,
+        autoplay: true,
+        
+    }
+    return <div className='content'>
+      
+    <Slider ref={setSlideRef} {...sliderSetting}>
+        {hotelCards.map((data,index ) =>(
+          <div className='card' key={index}>
+          <div className='card-header'>
+              <h2>{hotelCards.title}</h2>
+              <img className='card-image' src={data.imageSrc} />
+              </div>
+              <div className='card-body'>
+              <ul>
+                  {data.features.map((feature, index) =>(
+                      <li key={index}>{feature}</li>
+                  ))}
+              </ul>
+              </div>
+              <button className='card-btn'>Buy Now</button>
+           
+          </div>  
+        ))}
+        
+    </Slider>
+    <div className='controls'>
+        <button onClick={slideRef?.slickPrev}>
+          left button
+        </button>
+        <button className='right' onClick={slideRef?.slickNext}>
+          right button
+        </button>
+      </div>
     </div>;
 }
 
